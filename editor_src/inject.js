737eb889fdb47708fa8ea982457475b5244fc6c0
синечка ♡ <63897347+ktnk-dev@users.noreply.github.com>
2026-02-16 13:44:46 -0800

    init

---
+++ editor_src/inject.js
@@ -0,0 +1,142 @@
+const inject_css = [
+`#le_preview {
+    font-family:  'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    >* {
+        margin: 0 0 10px !important;
+    }
+    blockquote {
+        border-left: solid 2px #999;
+        margin: 0;
+        padding: 15px 20px;
+        font-family: 'Times New Roman', Times, serif;
+        font-style: italic;
+
+        p {
+            margin: 0;
+        }
+    }
+
+    table {
+        width: 100%;
+        border-spacing: 0px;
+        display: block;   /* Allows the overflow property to work directly on the table */
+        overflow-x: auto; /* Adds the horizontal scrollbar */
+        white-space: nowrap; /* Prevents text from wrapping within cells */
+        width: 100%; 
+        thead {
+            tr {
+                gap: 0;
+                th {
+                    border-bottom: solid 2px #999;
+                    padding: 4px 8px;
+                }
+            }
+        }
+        tbody {
+            tr {
+                td {
+                    padding: 5px 10px;
+                    border-bottom: solid 1px #888;
+                }
+            }
+        }
+    }
+    .markdown-alert-note {
+        /* --c: 184; */
+        --c: -230;
+        --mdc: hsl(130, 60%, 52%);
+        --mdb: hsl(184, 50%, 20%, .9);
+        --mde: hsla(184, 60%, 21%, .6);
+    }
+    .markdown-alert-tip {
+        --c: 214;
+        --mdc: hsl(214, 93%, 62%);
+    }
+    .markdown-alert-important {
+        --c: 264;
+        --mdc: hsl(262, 90%, 73%);
+    }
+    .markdown-alert-warning {
+        --c: 35;
+        --mdc: hsl(35, 72%, 48%);
+    }
+    .markdown-alert-caution {
+        --c: 10;
+        --mdc: hsl(3, 93%, 63%);
+    }
+    
+    .markdown-alert {
+        /* --text: hsla(var(--c), 100%, 90%);
+        --br: hsla(var(--c), 80%, 60%);
+        --bg1: hsla(var(--c), 60%, 30%, .9);
+        --bg2: hsla(var(--c), 60%, 30%, .7); */
+
+        --text: hsla(var(--c), 90%, 15%);
+        --br: hsla(var(--c), 80%, 60%);
+        --bg1: hsla(var(--c), 90%, 77%, .95);
+        --bg2: hsla(calc(var(--c) - 40), 60%, 70%, .8);
+
+        background: linear-gradient(
+            145deg,
+            var(--bg1),
+            var(--bg2)
+        );
+        color: var(--text);
+        display: flex;
+        flex-direction: column;
+        padding: 15px 15px 15px;
+        gap: 10px;
+        
+        /* border-left: solid 3px var(--br); */
+        /* border-radius: 0 15px 15px 0 ; */
+        border-radius: 15px;
+
+        .markdown-alert-title {
+            display: flex;
+            align-items: center;
+            gap: 7px;
+            margin: 0;
+            font-weight: 500;
+            color: var(--text);
+            svg {
+                fill: var(--text) !important;
+                width: 16px !important;
+                height: 16px !important;
+                margin: 0 !important;
+            }
+            
+        }
+        p:not(.markdown-alert-title) {
+            margin: 0;
+        }
+    }
+
+    img {
+        width: 100%;
+        border-radius: 15px;
+    }
+
+    hr {
+        opacity: 0;
+        margin: 20px 0 !important;
+    }
+    pre {
+        padding: 15px;
+        border-radius: 10px;
+        background: linear-gradient(
+            140deg,
+            #333,
+            #292929
+        );
+        color: #fff;
+        font-size: 14px;
+        user-select: all;
+        overflow-x: auto;
+        scrollbar-color: #999 #292929 ;
+        code {
+            font-weight: normal;
+        }
+
+    }
+}`
+]
---
+++ editor_src/main.js
@@ -0,0 +1,86 @@
+const get_css = () => inject_css.join()
+const update = () => {
+    le_preview.innerHTML = marked.use(markedAlert()).parse(
+        input.value
+    )
+
+    const css = get_css()
+    const v = () => document.querySelector('#cssinject').outerHTML = '<style id="cssinject">'+css+'</style>'
+    try {
+        v()
+    } catch (error) {
+        document.head.innerHTML += `<style id="cssinject"></style>`
+        v()
+    }
+    const translate = {
+        'Note': 'Запомни',
+        'Tip': 'На заметку',
+        'Important': 'Важно',
+        'Warning': 'Внимание',
+        'Caution': 'Осторожнее'
+    }
+    document.querySelectorAll('.markdown-alert-title').forEach(e => {
+        Object.keys(translate).map(k => e.innerHTML = e.innerHTML.replace(k, translate[k]))
+    })
+}  
+
+var theme_id = 0
+const themes = {
+    background: ['#222', '#fff'],
+    color: ['#fff', '#222']
+}
+const toggletheme = () => {
+    theme_id += 1
+    theme_value = theme_id%2
+    const rootElement = document.documentElement;
+    Object.keys(themes).map(key => {
+        rootElement.style.setProperty(`--${key}`, themes[key][theme_value])
+    })
+    rootElement.style.setProperty('--main-color', '#ff5733');
+}
+
+function copyToClipboard(text) {
+  // 1. Create a temporary textarea
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+  
+  // 2. Ensure it's not visible or disruptive
+  textArea.style.position = "fixed";
+  textArea.style.left = "-9999px";
+  textArea.style.top = "0";
+  document.body.appendChild(textArea);
+  
+  // 3. Select and copy
+  textArea.focus();
+  textArea.select();
+  
+  try {
+    const successful = document.execCommand('copy');
+    console.log(successful ? 'Copied!' : 'Copy failed');
+  } catch (err) {
+    console.error('Unable to copy', err);
+  }
+  
+  // 4. Clean up
+  document.body.removeChild(textArea);
+}
+
+
+const copyhtml = () => {
+    copyToClipboard(
+        `
+<style>${get_css()}</style>
+${le_preview.outerHTML}
+<script>
+window.addEventListener('load', () => document.head.innerHTML += \`<style>${get_css()}</style>\`)
+</script>
+`
+    )
+}
+
+const copyprompt = () => copyToClipboard(prompt_text)
+const paste = async () => {
+    // input.focus()
+    input.value = await navigator.clipboard.readText()
+    update()
+}
